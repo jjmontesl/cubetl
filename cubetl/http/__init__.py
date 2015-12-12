68817c4f504a6452667ff225d5cd0f55645ec0ea
Jose Juan Montes <jjmontes@gmail.com>
2015-12-12 20:16:12 +0100

    Added retry option to http request component.

--- cubetl/http/__init__.py
+++ cubetl/http/__init__.py
@@ -20,6 +20,7 @@ class HttpReader(FileReader):
     url = None
 
     user_agent = None
+    attempts = 3
 
     def initialize(self, ctx):
 
@@ -42,9 +43,16 @@ class HttpReader(FileReader):
             headers['User-Agent'] = self.user_agent
 
         req = urllib2.Request(url, headers=headers)
-        response = urllib2.urlopen(req)
-        html = response.read()
-        m[self.name] = html
+
+        attempt_count = 0
+        html = None
+        while attempt_count < self.attempts:
+            try:
+                attempt_count += 1
+                response = urllib2.urlopen(req)
+                html = response.read()
+            except Exception as e:
+                logger.warn("Could not retrieve HTTP document (attempt %d/%d): %s " % (attempt_count, self.attempts, e))
 
         # Encoding
         """
@@ -53,5 +61,10 @@ class HttpReader(FileReader):
         m["_encoding"] = encoding
         """
 
-        yield m
+        if html is not None:
+            m[self.name] = html
+            yield m
+        else:
+            logger.error("Coudl not retrieve HTTP document in %d attempts (blocking message)." % (self.attempts))
+
 
